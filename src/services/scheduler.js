cb20f83a596c9a972a4cb9f298f839dcad3cfad5
Huy Nguyen <huyy.802@gmail.com>
2024-04-09 15:52:58 +0700

    fix: missing import in scheduler, now working perfect

--- src/services/scheduler.js
+++ src/services/scheduler.js
@@ -1,10 +1,16 @@
 import Subscription from "../models/subscription.js";
 import cron from "node-cron";
+import axios from "axios";
 console.log("Schedule is working");
+import dotenv from "dotenv";
+import { TransporterService } from "./transporter.js";
+dotenv.config();
+const API_KEY = process.env.WEATHER_API_KEY;
+const WEATHER_API_BASE_URL = process.env.WEATHER_API_BASE_URL;
 const sendNewInfo = async () => {
   try {
     const subscriptions = await Subscription.find({}, "email location");
-
+    console.log("Subscriptions: " + subscriptions);
     for (const { email, location } of subscriptions) {
       const response = await axios.get(
         `${WEATHER_API_BASE_URL}current.json?key=${API_KEY}&q=${location}`
@@ -13,7 +19,7 @@ const sendNewInfo = async () => {
       if (response.status === 200) {
         const weatherData = response.data;
         const mailOptions = {
-          from: "Huy Weather <huyy.802@gmail.com>",
+          from: "Weather <huyy.802@gmail.com>",
           to: email,
           subject: "Weather - Thời tiết hôm nay",
           text: `Weather update for ${location}: ${JSON.stringify(
